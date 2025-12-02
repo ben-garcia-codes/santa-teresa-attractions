8e361e030f42e920ea7a85c36c402da2faba0d11
ben-garcia-codes <garciabe@cua.edu>
2025-12-02 13:35:44 -0500

    Final GA fix: Localhost exclusion applied

---
+++ santateresa3/main.js
@@ -0,0 +1,5 @@
+console.log("main.js is loaded!");
+alert("Welcome to Santa Teresa!");
+const menuItemsHTMLcollection = document.getElementsByClassName("Header");
+console.log(menuItemsHTMLcollection)
+
